a5b1e50408cd691cc9904a474ca1c9219e6e4174
GoodNyong <gy990213@naver.com>
2025-09-16 10:51:24 +0900

    두 수를 나눈 몫

---
+++ src/solution/Q03.java
@@ -0,0 +1,15 @@
+package solution;
+
+public class Q03 {
+    public static  void main(String[] args) {
+        int n1 = (int)(Math.random() * 101);
+        int n2 = (int)(Math.random() * 101);
+        System.out.println("n1 : " + n1);
+        System.out.println("n2 : " + n2);
+        System.out.println("result : " + solution(n1, n2));
+    }
+
+    public static int solution(int n1, int n2){
+        return (n1 / n2);
+    }
+}
